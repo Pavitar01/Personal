68930a09786793bef4cf8040542d12721ed26d54
Pavitar Singh <71433170+Pavitar01@users.noreply.github.com>
2022-09-11 15:26:11 +0530

    Add files via upload

---
+++ newebjava.js
@@ -0,0 +1,28 @@
+
+
+
+
+function hover()
+{
+document.getElementById("para1").innerHTML="<button>Read More..</button>";
+
+}
+
+function hover2()
+{
+document.getElementById("para2").innerHTML="<button>Read More..</button>";
+
+}
+
+
+function hover3()
+{
+document.getElementById("para3").innerHTML="<button>Read More..</button>";
+
+}
+
+function hover4()
+{
+document.getElementById("para4").innerHTML="<button>Read More..</button>";
+
+}
